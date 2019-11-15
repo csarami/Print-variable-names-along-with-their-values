7f4c2979f84e042a0b66122413ab70d3c1ef61b9
Chekad Sarami <csarami@gmail.com>
2019-11-15 12:38:03 -0500

    Change function name

--- print_VriableNames_with_values.py
+++ print_VriableNames_with_values.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
     def printv(variables, sep =''):
     ''' print variables names with their values
-        example: x = 1, y = 10; printV(x,y>10)
+        example: x = 1, y = 10; vprint(x,y>10)
         prints the following:
         x = 1
         y>10 = False'''
 
-    def printv(variables, sep ='\n'):
+    def vprint(variables, sep ='\n'):
     vars = variables.split(',')
     for var in vars:
       print(var.strip(), '=', repr(eval(var)), end = sep)
